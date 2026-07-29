91c8d13ee329d656b1722edf6626bab401107910
Evelyn Almeida <evelynqffreitas@gmail.com>
2026-07-29 12:10:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,45 @@ loginForm.addEventListener('submit', (e) => {
   // Redireciona para a home após efetuar o "login"
   window.location.href = 'home.html';
 });
+
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+// Botões Desktop
+if (registerBtn && loginBtn) {
+  registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+  });
+
+  loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+  });
+}
+
+// Botões Abas Mobile
+const tabLogin = document.getElementById('tabLogin');
+const tabRegister = document.getElementById('tabRegister');
+
+if (tabLogin && tabRegister) {
+  tabLogin.addEventListener('click', () => {
+    container.classList.remove('active');
+    tabLogin.classList.add('active');
+    tabRegister.classList.remove('active');
+  });
+
+  tabRegister.addEventListener('click', () => {
+    container.classList.add('active');
+    tabRegister.classList.add('active');
+    tabLogin.classList.remove('active');
+  });
+}
+
+// Redirecionamento ao Enviar Login/Cadastro para a Home
+const forms = document.querySelectorAll('form');
+forms.forEach(form => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    window.location.href = 'home.html';
+  });
+});
